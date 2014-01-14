04d0ffe354d927c5b79046eab0a90bdc960149c5
pkiulian <pkiulian@gmail.com>
2014-01-14 14:57:00 +0100

    Update and rename ScientificConferenceTree.java to ScientificConference.java

--- ScientificConference.java
+++ ScientificConference.java
@@ -6,7 +6,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Iterator;
 
-public class ScientificConferenceTree{
+public class ScientificConference{
 		
 	static int getFinalResult(ArrayList<Integer> SumsV){
 		int finalResult = 0;
